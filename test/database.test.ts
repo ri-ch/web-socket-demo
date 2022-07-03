f4b98491bbef23cc6376c79dfbda42bb7fade8cd
Richard Crawley <richard.crawley@protonmail.com>
2022-07-03 15:28:27 +0100

    Initial commit

---
+++ test/database.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Database from '../lib/database-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/database-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Database.DatabaseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
